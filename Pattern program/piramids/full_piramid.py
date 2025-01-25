b506a9f4c133996f462aba33926744edfbe8f11a
Prathampal Singh Bhamrah <147434371+prathampalsingh@users.noreply.github.com>
2025-01-25 06:29:25 +0530

    changhes

--- Pattern program/piramids/full_piramid.py
+++ Pattern program/piramids/full_piramid.py
@@ -3,9 +3,12 @@ def full_piramid(n):
         #half reverse left
         for col in range(row,n):
               print(' ', end="")
-        #half left
+        #half left without row + 1 to get the top in next for loop
+        for col in range(row ):
+              print(col, end="")
+        #half left 
         for col in range(row+1 ):
-              print(col, end=" ")
+              print(col, end="")
         print("")
 
 
