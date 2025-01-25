0a03db92a9178dca7cf96b6f18f67ce0c8ce36af
Prathampal Singh Bhamrah <147434371+prathampalsingh@users.noreply.github.com>
2025-01-25 06:35:01 +0530

    changes

--- Pattern program/piramids/full_reverse.py
+++ Pattern program/piramids/full_reverse.py
@@ -3,6 +3,9 @@ def half_pyramid(n):
         #half-left 
         for col in range(row):
             print(' ', end="")
+        #half reverse left with n - 1 so the next for loop can complete the bottom 4
+        for col in range(row,n-1):
+              print(col, end="")
         #half reverse left
         for col in range(row,n):
               print(col, end="")
