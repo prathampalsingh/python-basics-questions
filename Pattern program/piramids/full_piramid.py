4bd4b529fe60d174feb69792d7e9381a6fa89dff
Prathampal Singh Bhamrah <147434371+prathampalsingh@users.noreply.github.com>
2025-01-25 06:24:26 +0530

    commit

--- Pattern program/piramids/full_piramid.py
+++ Pattern program/piramids/full_piramid.py
@@ -1,7 +1,9 @@
 def full_piramid(n):
     for row in range(n):
+        #half reverse left
         for col in range(row,n):
               print(' ', end="")
+        #half left
         for col in range(row+1 ):
               print(col, end=" ")
         print("")
